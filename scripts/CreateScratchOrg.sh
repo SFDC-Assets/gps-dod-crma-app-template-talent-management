250561fcfb95447ee531e9c8bababc73af1498c9
Navaforce <dnava@salesforce.com>
2023-05-23 14:58:46 -0400

    Changed company case (test)

--- scripts/CreateScratchOrg.sh
+++ scripts/CreateScratchOrg.sh
@@ -2,7 +2,7 @@
 #
 #  Creates a new scratch org and populates it with sample data.
 #
-#  Copyright (c) 2021-2023, salesforce.com, inc.
+#  Copyright (c) 2021-2023, Salesforce.com, inc.
 #  All rights reserved.
 #  SPDX-License-Identifier: BSD-3-Clause
 #  For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
